265f5e1ccd9cc7da3f3ba215385879fd0ea7c75c
jeaps17 <jeaps17@gmail.com>
2023-04-25 14:42:19 -0600

    commit on 'main'

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_rC7H6zSQw',
+        userPoolClientId: '4o627g808lq954f0iemrmp6nmb',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
